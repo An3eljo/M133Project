43145183e3025678cf1466e015ef538315f69ebd
andri.joos69 <andri.joos.tbz@gmail.com>
2018-06-15 14:14:56 +0200

    aj: add login

--- M133Project/Default.aspx.cs
+++ M133Project/Default.aspx.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace M133Project
 {
     public partial class _Default : Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
         {
+            MySqlConnection connection = new MySqlConnection("server=localhost;Port=3306;database=test0;uid=root;SslMode=0;password=");
 
+            //open the connection
+            connection.Open();
+
+            //close the connection
+            connection.Close();
         }
     }
 }
